37e211ba1840d6b066655c20c2beb3bb8a025049
Lilian HERBELOT <lherbelo@e2r9p20.42.fr>
2016-10-10 12:16:30 +0200

    debug parse

--- src/parse.c
+++ src/parse.c
@@ -6,37 +6,37 @@
 /*   By: lherbelo <lherbelo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/24 16:23:45 by lherbelo          #+#    #+#             */
-/*   Updated: 2016/10/07 09:52:24 by lherbelo         ###   ########.fr       */
+/*   Updated: 2016/10/10 12:15:44 by lherbelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/wolf.h"
 
-void		ft_place(t_mlx *m, int xmax, int ymax)
+void		ft_place(t_mlx *m)
 {
-	int x;
-	int y;
+	int		x;
+	int		y;
+	int		search;
 
-	DEBUG
+	search = 1;
 	x = 0;
-	while (x <= xmax)
+	while (x < m->w->x && search)
 	{
 		y = 0;
-		while (y <= ymax)
+		while (y < m->w->y && search)
 		{
-			if (m->w->map[x][y] == 0 && m->w->map[x + 1][y] == 0 &&
-				m->w->map[x - 1][y] == 0 && m->w->map[x][y + 1] == 0 &&
-				m->w->map[x][y - 1] == 0)
+			if (m->w->map[x][y] == 0 && m->w->map[x + 1][y]  == 0 &&
+					m->w->map[x - 1][y] == 0 && m->w->map[x][y + 1] == 0 &&
+					m->w->map[x][y - 1] == 0)
 			{
-				m->w->posx = x;
-				m->w->posy = y;
+				m->w->posx = x + 0.5;
+				m->w->posy = y + 0.5;
+				search = 0;
 			}
-			else
-				y++;
+			y++;
 		}
-	x++;
+		x++;
 	}
-	DEBUG
 }
 
 void		ft_full(t_mlx *m, char *av)
@@ -50,6 +50,7 @@ void		ft_full(t_mlx *m, char *av)
 	while (get_next_line(fd, &line))
 	{
 		m->w->y = 0;
+		printf("%s\n", line);
 		tmp = ft_strsplit(line, ' ');
 		while (tmp[m->w->y] != NULL)
 		{
@@ -58,7 +59,7 @@ void		ft_full(t_mlx *m, char *av)
 		}
 		m->w->x++;
 	}
-	ft_place(m, m->w->x, m->w->y);
+	close(fd);
 }
 
 int			**ft_size(t_mlx *m, int x, int y)
@@ -67,7 +68,7 @@ int			**ft_size(t_mlx *m, int x, int y)
 
 	i = 0;
 	m->w->map = (int **)ft_memalloc(sizeof(int *) * x);
-	while (i < m->w->y)
+	while (i < x)
 	{
 		m->w->map[i] = (int *)ft_memalloc(sizeof(int *) * y);
 		i++;
@@ -82,15 +83,21 @@ void		ft_parse(t_mlx *m, char *av)
 	int		fd;
 
 	fd = open(av, O_RDONLY);
-	m->w->y = 0;
+	m->w->x = 0;
 	while (get_next_line(fd, &line))
 	{
-		m->w->x = 0;
+		m->w->y = 0;
 		tmp = ft_strsplit(line, ' ');
-		while (tmp[m->w->x] != NULL)
-			m->w->x++;
-		m->w->y++;
+		while (tmp[m->w->y] != NULL)
+			m->w->y++;
+		m->w->x++;
 	}
+	close(fd);
+	DEBUG
 	ft_size(m, m->w->x + 1, m->w->y + 1);
+	DEBUG
 	ft_full(m, av);
+	DEBUG
+	ft_place(m);
+	DEBUG
 }
