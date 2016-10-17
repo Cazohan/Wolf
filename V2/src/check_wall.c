0ac9a38a50ff592027b2fde746d3a60764ea382d
Lilian HERBELOT <lherbelo@e3r10p23.42.fr>
2016-10-17 16:01:31 +0200

    modif exec, delete useless fonction

--- V2/src/check_wall.c
+++ V2/src/check_wall.c
@@ -6,34 +6,11 @@
 /*   By: lherbelo <lherbelo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/10 14:10:26 by lherbelo          #+#    #+#             */
-/*   Updated: 2016/10/17 15:45:19 by lherbelo         ###   ########.fr       */
+/*   Updated: 2016/10/17 15:59:23 by lherbelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/wolf.h"
-
-void		ft_check_wall(t_mlx *m)
-{
-	int		x;
-	int		y;
-
-	x = 0;
-	while (x < m->w->x)
-	{
-		y = 0;
-		while (y < m->w->y)
-		{
-			if (x == 0 || (x == m->w->x - 1))
-				if (m->w->map[x][y] == 0)
-					ft_error(m, 3);
-			if (y == 0 || (y == m->w->y - 1))
-				if (m->w->map[x][y] == 0)
-					ft_error(m, 3);
-			y++;
-		}
-		x++;
-	}
-}
 
 void		ft_color_wall(t_mlx *m, t_rgb *c)
 {
