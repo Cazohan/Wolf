1f38be1df208fc070f145d327d3c6a3658708680
Lilian HERBELOT <lherbelo@e2r9p20.42.fr>
2016-10-25 11:27:32 +0200

    regroupement de fonctions

--- src/get_frame.c
+++ src/get_frame.c
@@ -6,7 +6,7 @@
 /*   By: lherbelo <lherbelo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/29 19:18:48 by lherbelo          #+#    #+#             */
-/*   Updated: 2016/10/17 15:44:42 by lherbelo         ###   ########.fr       */
+/*   Updated: 2016/10/25 11:26:11 by lherbelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,32 @@ void					get_frame(t_mlx *m)
 		m->w->move_speed = m->w->frame * 0.020;
 	if (m->w->run == 0)
 		m->w->move_speed = m->w->frame * 0.005;
+}
+
+void		ft_color_wall(t_mlx *m, t_rgb *c)
+{
+	if (m->w->side == 0)
+	{
+		if (m->w->stepx == -1)
+			select_color(c, 120, 150, 210);
+		else
+			select_color(c, 120, 210, 110);
+	}
+	else if (m->w->side == 1 && m->w->hit == 1)
+	{
+		if (m->w->stepy == -1)
+			select_color(c, 20, 70, 31);
+		else
+			select_color(c, 90, 10, 10);
+	}
+}
+
+void		ft_fixc(t_mlx *m, t_rgb *c)
+{
+	if (m->w->map[m->w->mapx][m->w->mapy] == 2)
+		select_color(c, 210, 210, 0);
+	else if (m->w->map[m->w->mapx][m->w->mapy] == 5)
+		select_color(c, 250, 110, 80);
+	else
+		ft_color_wall(m, c);
 }
